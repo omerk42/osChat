ff07f0e141dce4ad9289ce5c1c68cd391ba73be9
omerk42 <omerkhanak42@gmail.com>
2023-06-06 04:45:06 +0300

    add chat ui

--- os_chat/chat/views.py
+++ os_chat/chat/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('message-input', '') 
+        print(user_input)
     return render(request , template_name = "chat/main.html")
